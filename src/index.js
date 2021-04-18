bbd14715e5a75330003e005174de4a1e1dbbffe2
vm-mishchenko <vm.mishchenko@gmail.com>
2021-04-17 18:25:56 -0700

    Update response message

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer(function (req, res) {
   res.setHeader("Content-Type", "application/json");
   res.writeHead(200);
-  res.end(`{"message": "This is Backend"}`);
+  res.end(`{"message": "This is the response from Backend service (https://backend-7tt2isxsdq-wl.a.run.app). "}`);
 });
 
 server.listen(8080);
